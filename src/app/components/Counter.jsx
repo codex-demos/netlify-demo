e691ed048240f394fa8f9b200fa5bbb32fbf5622
Lewis Benson <tm.lewis.benson@gmail.com>
2024-04-08 20:47:27 -0400

    add counter and sass

---
+++ src/app/components/Counter.jsx
@@ -0,0 +1,21 @@
+'use client';
+import { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div
+        onClick={() => {
+          setCount((previousCount) => previousCount + 1);
+          setCount((previousCount) => previousCount + 1);
+        }}
+        className="btn-s btn-s-primary"
+      >
+        +
+      </div>
+      <div>Count: {count}</div>
+    </>
+  );
+}
---
+++ src/app/components/WelcomeTest.jsx
@@ -0,0 +1,3 @@
+export default function WelcomeTest() {
+  return <div>WelcomeTest</div>;
+}
